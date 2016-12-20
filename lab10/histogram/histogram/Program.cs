5440c847dad27b8c2018a053feebf706338b3f87
Artur Koliński <secorek@gmail.com>
2016-12-20 10:51:38 +0100

    Added histogram on words with dictionary

---
+++ lab10/histogram/histogram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace histogram
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = "test artur test test artur lol aa bb lolek lol";              
+            foreach (var item in HistogramWords(input))
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+            Console.ReadKey();
+        }
+
+        static Dictionary<string, int> HistogramWords(string input)
+        {
+            var words = new Dictionary<string, int>(StringComparer.CurrentCultureIgnoreCase);
+            var wordPattern = new Regex(@"\w+");
+            foreach (Match match in wordPattern.Matches(input))
+            {
+                int currentCount = 0;
+                words.TryGetValue(match.Value, out currentCount);
+                currentCount++;
+                words[match.Value] = currentCount;
+            }
+            return words;
+        }
+    }
+}
