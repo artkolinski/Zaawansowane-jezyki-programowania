0b00cc3ed1c174c4a2ff209d195198356ecb553f
Artur Koliński <secorek@gmail.com>
2016-12-20 11:26:08 +0100

    Lab 11 adding fractions by overloaded operator and by function with time measure

---
+++ Lab11/ulamki/ulamki/Fraction.cs
@@ -0,0 +1,52 @@
+﻿namespace ulamki
+{
+    public class Fraction
+    {
+        int Numerator { get; set; }
+        int Denominator { get; set; }
+        public Fraction()
+        {            
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        public static Fraction operator + (Fraction a, Fraction b)
+        {
+            Fraction result = new Fraction();
+            if (a.Denominator == b.Denominator)
+            {
+                result.Numerator = a.Numerator + b.Numerator;
+                result.Denominator = a.Denominator;
+            }
+            else
+            {
+                result.Denominator = a.Denominator*b.Denominator;
+                result.Numerator = ((result.Denominator/a.Denominator)*a.Numerator) + ((result.Denominator/b.Denominator)* b.Numerator);
+            }
+            return result;
+        }
+
+        public static Fraction Add(Fraction a, Fraction b)
+        {
+            Fraction result = new Fraction();
+            if (a.Denominator == b.Denominator)
+            {
+                result.Numerator = a.Numerator + b.Numerator;
+                result.Denominator = a.Denominator;
+            }
+            else
+            {
+                result.Denominator = a.Denominator * b.Denominator;
+                result.Numerator = ((result.Denominator / a.Denominator) * a.Numerator) + ((result.Denominator / b.Denominator) * b.Numerator);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "(" + Numerator + " / " + Denominator + ")";
+        }
+    }
+}
---
+++ Lab11/ulamki/ulamki/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Diagnostics;
+
+namespace ulamki
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var stopwatch = new Stopwatch();
+            Fraction a = new Fraction(1, 2);
+            Fraction b = new Fraction(2, 3);
+            stopwatch.Start();
+                Console.Write($"Adding by overload operator {a} + {b} = ");
+                Console.WriteLine(a+b);
+                stopwatch.Stop();
+                Console.WriteLine("Time adding by overload operator = " + stopwatch.ElapsedMilliseconds + " ms");
+                stopwatch.Reset();
+            stopwatch.Start();
+                Console.Write($"Adding by function {a} + {b} = ");
+                Console.WriteLine(Fraction.Add(a,b));
+                stopwatch.Stop();
+                Console.WriteLine("Time adding by function = " + stopwatch.ElapsedMilliseconds + " ms");
+                stopwatch.Reset();
+            Console.ReadKey();
+        }
+    }
+}
