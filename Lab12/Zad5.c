de0c6f02ecef67500d0236dcc35eaa142e46ebf7
Artur Koliński <secorek@gmail.com>
2017-01-22 16:08:32 +0100

    Preparing to test for calculating addresses, added calculations and notes

--- Lab12/Zad5.c
+++ Lab12/Zad5.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
 
 int adrStart2(int wartoscStart,int n, int m, int p, int i, int j, int k, int rozmiarStruktury){
+// tab[n][m][p]  <- zadeklarowana tablica
+// tab[10][100][20]
+// -----------------
+// tab[i][j][k]  <- tablica ktora liczymy
+// tab[2][3][4]
 int obl = wartoscStart + ((i*m+j)*p+k)*rozmiarStruktury;
 return (obl);
 };
 
-int adrStartComplex(int wartoscStart, int i, int j, int k, int rozmiarStruktury,int c, int kk){
-// tab[n][m][p]
-int n=10, m=100, p=20;
-int obl = wartoscStart + ((i*m+j)*p+k)*rozmiarStruktury;
-// .zmienna[c][kk]
-char b[10][15];
-int obl2 =  adrStart2dimm(obl,10,c,kk,sizeof(b));
-return (obl2);
-};
-
-int adrStart2dimm(int wartoscStart,int calkIloscKolumn, int kolumna, int wiersz, int rozmiarStruktury){
-int obl = wartoscStart + (kolumna*calkIloscKolumn + wiersz)*rozmiarStruktury;
-return (obl);
+struct ABC {
+    double a;
+    char b[10][15];
+    int c[12];
 };
 
 int main()
 {
-  // tab[n][m][p]
-  // tab[i][j][k]
+  struct ABC abc;
+  printf("Size ABC = %d", sizeof(abc));
   printf("\n\n - Funkcja adr startowy tab[2][3][4] = %d \n", adrStart2(1000,10,100,20,2,3,4,208));
-  int adrStart = adrStartComplex(1000,2,3,4,208,2,3);
-  printf("b) 846353");
-  printf("\n - adresowanie tab[2][3][4].b[2][3] : %d - %d \n", adrStart, adrStart+208);
-  /* -- Notatki
-   846312 - 846319
-   846320 - 846472 (152)
-   tab[2][3][4].b[2][3] - 846353
-   b[2][3] 846519
-   poprawna odp. b) 846353
-  */
   return(0);
 }
